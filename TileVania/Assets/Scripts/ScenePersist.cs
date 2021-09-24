740358a84619ff9b5c7530985d4030e813a71ee2
ytii <derekwaderobert@gmail.com>
2021-09-24 13:23:28 -0400

    Fixed Scene Persist

--- TileVania/Assets/Scripts/ScenePersist.cs
+++ TileVania/Assets/Scripts/ScenePersist.cs
@@ -6,32 +6,57 @@ using UnityEngine.SceneManagement;
 public class ScenePersist : MonoBehaviour
 {
 
-    int startingSceneIndex;
+    int sceneIndex;
 
-    void Awake()
-    {
-        int numScenePersists = FindObjectsOfType<ScenePersist>().Length;
-        if (numScenePersists > 1)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-    }
+    // void Awake()
+    // {
+    //     int numScenePersists = FindObjectsOfType<ScenePersist>().Length;
+    //     if (numScenePersists > 1)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    //     else
+    //     {
+    //         DontDestroyOnLoad(gameObject);
+    //     }
+    // }
+
+        private void Awake()
+            {
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //get active scene number
+                
+                ScenePersist[] persists = FindObjectsOfType<ScenePersist>(); //Create an array to hold scene persist objects
+                
+                foreach (var persist in persists) //Check for objects that are a part of scene persist
+            {
+                if (persist != this) //Check for scene persist in the scene
+                {
+                    if (persist.sceneIndex == currentSceneIndex)
+                    {
+                        Destroy(gameObject); //destroy persist object and return immediately (anything that is already in room upon awake)
+                        return;
+                    }
+                    else
+                    {
+                        Destroy(persist.gameObject); //destroy if doesn't have the current scene index
+                    }
+                }
+            }
+            sceneIndex = currentSceneIndex; //set sceneIndex variable to current scene index
+            DontDestroyOnLoad(gameObject); //create dont destroy on load object after scene index set correctly and all prior scene persists have been destroyed
+            }
 
     // Start is called before the first frame update
     void Start()
     {
-        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (currentSceneIndex != startingSceneIndex)
+        if (currentSceneIndex != sceneIndex)
         {
             Destroy(gameObject);
         }
